054c7a9e7a1f535e9c80be6f27a3d7cce867fee3
0x1 <kevin.francart3@gmail.com>
2026-07-06 18:58:50 +0200

    feat: migrate FFI to exports array format

--- src/Effect/Console.php
+++ src/Effect/Console.php
@@ -1,3 +1,6 @@
 <?php
 
-$Effect_Console_log = function($s) { return function() use(&$s) { print($s . "\n"); }; };
+$log = function($s) use (&$log) { return function() use(&$s) { print($s . "\n"); }; };
+
+$exports['log'] = $log;
+return $exports;
